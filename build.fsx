c8ca748ce4e609cedae25e611dd822d741e04c10
Jon <jwood803@users.noreply.github.com>
2014-11-12 16:41:44 -0600

    Add FsReveal and initial output

---
+++ build.fsx
@@ -0,0 +1,64 @@
+#I @"packages/FsReveal/fsreveal/"
+#I @"packages/FAKE/tools/"
+#I @"packages/RazorEngine/lib/net40/"
+
+#r "FakeLib.dll"
+
+#load "fsreveal.fsx"
+
+open FsReveal
+open Fake
+open System.IO
+
+let outDir = "output"
+
+Target "Clean" (fun _ ->
+    CleanDirs [outDir]
+)
+
+let copyPics() =
+    !! "pics/*.*"
+    |> CopyFiles (outDir @@ "images")
+
+let generateFor (file:FileInfo) =    
+    copyPics()
+    let rec tryGenerate trials =
+        try
+            let outputFileName = file.Name.Replace(file.Extension,".html")
+            match file.Extension with   
+            | ".md" ->  FsReveal.GenerateOutputFromMarkdownFile outDir outputFileName file.FullName
+            | ".fsx" -> FsReveal.GenerateOutputFromScriptFile outDir outputFileName file.FullName
+            | _ -> ()
+        with 
+        | exn when trials > 0 -> tryGenerate (trials - 1)
+        | exn -> tracefn "Could not generate slides for %s. %s" file.FullName exn.Message
+
+    tryGenerate 3
+
+Target "GenerateSlides" (fun _ ->
+    !! "slides/*.md"
+      ++ "slides/*.fsx"
+    |> Seq.map fileInfo
+    |> Seq.iter generateFor
+)
+
+Target "KeepRunning" (fun _ ->
+    use watcher = new FileSystemWatcher(DirectoryInfo("slides").FullName,"*.*")
+    watcher.EnableRaisingEvents <- true
+    watcher.Changed.Add(fun e -> fileInfo e.FullPath |> generateFor)
+    watcher.Created.Add(fun e -> fileInfo e.FullPath |> generateFor)
+    watcher.Renamed.Add(fun e -> fileInfo e.FullPath |> generateFor)
+
+    traceImportant "Waiting for slide edits. Press any key to stop."
+
+    System.Console.ReadKey() |> ignore
+
+    watcher.EnableRaisingEvents <- false
+    watcher.Dispose()
+)
+
+"Clean"
+  ==> "GenerateSlides"
+  ==> "KeepRunning"
+
+RunTargetOrDefault "KeepRunning"
